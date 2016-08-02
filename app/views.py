4445ff15a5f5d87bdf41032034710d0bfd76f2ba
E. Ciubarov <me@ciubarov.ru>
2016-08-02 23:23:37 +0300

    vk4

--- app/views.py
+++ app/views.py
@@ -4,12 +4,7 @@ from api.vk import VKApi
 from django.http import HttpResponseForbidden, HttpResponse
 
 def home(request):
-    message = 'test'
-    vk_social = request.user.social_auth.get(provider='vk-oauth2')
-    vk_token = vk_social.extra_data['access_token']
-    vk_api = VKApi(vk_social.uid,vk_token)
-    resp = vk_api.wall_post(message)
-    return render(request, 'app/home.html', {'resp': resp})
+    return render(request, 'app/home.html', {})
 
 def show_article(request, article_id):
     article = get_object_or_404(Article, id=article_id)
